2ec1d5a6496cee6d1000bbe4635aa6d3ed83ff51
nugaon <toth.viktor.levente@gmail.com>
2021-10-18 19:40:51 +0200

    feat: swarm bee provider

---
+++ src/providers/bee.tsx
@@ -0,0 +1,29 @@
+import { Bee } from '@ethersphere/bee-js'
+import { createContext, ReactChild, ReactElement, useState } from 'react'
+
+interface ContextInterface {
+  bee: Bee
+}
+
+const initialValues: ContextInterface = {
+  bee: new Bee('https://bee-9.gateway.ethswarm.org'),
+}
+
+export const Context = createContext<ContextInterface>(initialValues)
+export const Consumer = Context.Consumer
+
+interface Props {
+  children: ReactChild | ReactChild[]
+}
+
+export function Provider({ children }: Props): ReactElement {
+  const [bee] = useState<Bee>(initialValues.bee)
+
+  return (
+    <Context.Provider value={{ bee }}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export default Provider

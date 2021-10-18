05289b9419713e4fb7c547a274a9268b2dbca90f
nugaon <toth.viktor.levente@gmail.com>
2021-10-18 19:42:32 +0200

    feat: add bee and menu elements to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css';
 import ChessTable from './chess-table';
+import Menu from './menu';
+import Bee from './providers/bee';
 import ChessEngine from './providers/chess-engine';
 
 function App() {
 
   return (
     <div className="App">
-      <ChessEngine>
-        <ChessTable></ChessTable>
-      </ChessEngine>
+      <Bee>
+        <ChessEngine>
+          <Menu></Menu>
+          <ChessTable></ChessTable>
+        </ChessEngine>
+        </Bee>
     </div>
   );
 }

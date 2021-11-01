0a6747b32a491832ab197cf1950e045c02689e13
nugaon <toth.viktor.levente@gmail.com>
2021-11-01 13:29:31 +0100

    feat: communicate with agent instead of ai

--- src/components/chess-table.tsx
+++ src/components/chess-table.tsx
@@ -1,17 +1,22 @@
-import { Square } from 'chess-types';
+import axios from 'axios';
+import { Move, ShortMove, Square } from 'chess-types';
 import Chessboard from 'chessboardjsx';
 import { useContext, useEffect, useState } from 'react';
-import { Context as ChessContext } from '../providers/chess-engine';
 import { Context as isAIcheckedContext } from '../providers/ai-checked';
-import { Context as BeeContext, zeroPostageId } from '../providers/bee';
-import { setSwarmHashToUrl, uploadString } from '../utils/swarm-game-data';
-
+import { Context as BeeContext } from '../providers/bee';
+import { Context as ChessContext } from '../providers/chess-engine';
 
 
 type Piece =
   'wP' | 'wN' | 'wB' | 'wR' | 'wQ' | 'wK' |
   'bP' | 'bN' | 'bB' | 'bR' | 'bQ' | 'bK'
 ;
+
+export async function sendGameEnd(history: string[]): Promise<void> {
+  await axios.post('http://localhost:3001/game-end', {
+    history
+  })
+}
 
 export default function ChessTable() {
 
@@ -24,7 +29,34 @@ export default function ChessTable() {
   const { bee } = useContext(BeeContext)
   const isAIchecked = useContext(isAIcheckedContext);
   const [checkMate, setCheckMate ] = useState(false)
-  const [winnder, setWinner] = useState('')
+  const [winner, setWinner] = useState<'w' | 'b' | 'd' | ' '>(' ')
+  //const { setChecked } = useContext(isAIcheckedContext)
+  const [stateHash, setStateHash] = useState('')
+  const [stateUrl, setStateUrl] = useState('')
+  const [check, setCheck] = useState<boolean>(false)
+  const [draw, setDraw] = useState<boolean>(false)
+  //const [checked, setCheckboxChecked ] = useState(false)
+  const [AIMove, setAIMove ] = useState('')
+
+  const move = (step: string | ShortMove): Move | null => {
+    const move = game.move(step)
+    if(!move) return move
+
+    // after move
+    if (game.in_checkmate()) {
+      sendGameEnd(game.history())
+      setCheckMate(true)
+      setWinner(game.history().length % 2 === 1 ? 'w' : 'b' )
+    } else if (game.in_stalemate() || game.in_draw()) {
+      sendGameEnd(game.history())
+      setDraw(true)
+      setWinner('d')
+    } else if(check !== game.in_check()) {
+      setCheck(game.in_check())
+    }
+
+    return move
+  }
 
   const squareStyling = ({ pieceSquare, history }: { pieceSquare: string | null, history: Array<any> }) => {
     const sourceSquare = history.length && history[history.length - 1].from;
@@ -79,27 +111,12 @@ export default function ChessTable() {
     setSquareStyles({...squareStyles, ...highlightStyles})
   };
 
-  //const { setChecked } = useContext(isAIcheckedContext)
-  const [stateHash, setStateHash] = useState('')
-  const [stateUrl, setStateUrl] = useState('')
-  //const [checked, setCheckboxChecked ] = useState(false)
-  const [AIMove, setAIMove ] = useState('')
-
-  if (AIMove!=='') {
+  if (AIMove !== '') {
     console.log("AI Move: "+AIMove)
 
-    game.move(AIMove)
+    move(AIMove)
     setFen(game.fen())
     setAIMove('')
-
-    if (AIMove.includes("#") || AIMove.includes("++") || AIMove.includes("=")) {
-      setCheckMate(true)
-      setWinner('AI')
-      //game.in_checkmate() ? setWinner('AI') : setWinner('Human')
-      console.log(game.in_checkmate())
-      console.log(game.history())
-      alert("  --== ALL HAIL H.A.L. ==--  ")
-    }
   }
   async function getAIMove(fen:any){
 
@@ -120,35 +137,55 @@ export default function ChessTable() {
         return  data.json})
   }
 
+  async function getAgentMove(fen:string, history: string[]): Promise<any>{
+    console.log('ask for agent moves at state', fen, history)
+
+    const response = await axios.post('http://localhost:3001/step', {
+      fen,
+      history
+    })
+
+    const { step, perpetrator } = response.data
+    console.log('agent move: perpetrator', perpetrator)
+
+    return step
+}
+
 
   async function applyAIMove(){
-
     await getAIMove(game.fen())
+  }
 
+  async function applyAgentMove() {
+    const fen = game.fen()
+    const history = game.history()
+    const response = await getAgentMove(fen, history)
+    console.log('response', response)
+
+    setAIMove(response)
   }
 
   const onDrop = ({ sourceSquare, targetSquare, piece }: { sourceSquare: Square, targetSquare: Square, piece: Piece }): void => {
 
     // see if the move is legal
-    let move = game.move({
+    let step = move({
       from: sourceSquare,
       to: targetSquare,
       promotion: 'q' // always promote to a queen for example simplicity
     });
 
     // illegal move
-    if (move === null) return;
+    if (step === null) return;
 
 
     if(isAIchecked.checked){
-      applyAIMove()
+      applyAgentMove()
     }
 
     setFen(game.fen())
     console.log(game.fen())
 
     setHistory(game.history({ verbose: true }))
-
   }
 
   const onMouseOverSquare = (square: Square) => {
@@ -185,17 +222,17 @@ export default function ChessTable() {
 
     if(!pieceSquare) return
 
-    let move = game.move({
+    let step = move({
       from: pieceSquare,
       to: square,
       promotion: 'q' // always promote to a queen for example simplicity
     });
 
     // illegal move
-    if (move === null) return;
+    if (step === null) return;
 
     if(isAIchecked.checked){
-      applyAIMove()
+      applyAgentMove()
     }
 
     setFen(game.fen())
@@ -205,14 +242,23 @@ export default function ChessTable() {
     setPieceSquare(null)
   };
 
-  //const onSquareRightClick = (square: Square) =>
-    //setSquareStyles({ [square]: { backgroundColor: 'deepPink' } })
-
   useEffect(() => {
     setFen(startingFen)
   }, [startingFen])
 
-  return (
+  return (<>
+    <div hidden={!checkMate}>
+    Check Mate!
+  </div>
+  <div hidden={!check}>
+    Check!
+  </div>
+  <div hidden={winner !== 'w' && winner !== 'b' }>
+    The winner is {winner === 'w' ? 'White' : 'Black'}
+  </div>
+  <div hidden={winner !== 'd' }>
+    Draw!
+  </div>
     <div style={{
       height: '100%',
       display: 'flex',
@@ -234,8 +280,8 @@ export default function ChessTable() {
         dropSquareStyle={dropSquareStyle}
         onDragOverSquare={onDragOverSquare}
         onSquareClick={onSquareClick}
-        //onSquareRightClick={onSquareRightClick}
       />
     </div>
+    </>
   )
 }
